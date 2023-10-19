10c13de82632481ab167cc9584c833a43fc75792
WojtowiczZuzanna <146974708+WojtowiczZuzanna@users.noreply.github.com>
2023-10-19 11:38:16 +0200

    Add files via upload

---
+++ 02-TypesAndVariables/02_15.py
@@ -0,0 +1,6 @@
+d_Celsius = int(input("Enter temperature (in Celsius degrees): "))
+
+d_Klevin = d_Celsius + 273
+d_Fahrenheit = d_Celsius * 9/5 + 32
+
+print(f"Temperature: {d_Celsius} in Celsius degrees equals {d_Klevin} in Kelvin degrees and {d_Fahrenheit} in Fahrenheit degrees.")
